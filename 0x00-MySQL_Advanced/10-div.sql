28860280a72ac6fc93d7dd8adff1ea1495209364
Emildawood123 <edawood997@gmail.com>
2024-03-20 17:45:25 -0400

    first fun

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,10 +1,12 @@
 -- create my first funcation in sql
 DELIMITER $$
 CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT
+DETERMINISTIC
 BEGIN
     IF b = 0
         THEN RETURN 0;
-    END IF;
-    RETURN a / b; 
+        ELSE RETURN a / b;
+    END IF; 
 END $$
 DELIMITER ;
