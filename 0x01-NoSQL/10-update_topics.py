fa7311d73242fe2fc957d3bffdab7b4725815eed
e <e@g.com>
2024-03-26 05:27:42 -0700

    add double quotation rounded

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -2,4 +2,4 @@
 """update topics"""
 def update_topics(mongo_collection, name, topics):
     """update topics"""
-    mongo_collection.update_many({"name": name}, {$set: {"topics": topics}})
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
