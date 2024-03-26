2f401606b63c36f91f332f14274613ffa0fda6bd
e <e@g.com>
2024-03-26 06:13:50 -0700

    Done

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""count method and status inide docs"""
+from pymongo import MongoClient
+
+
+def count_methods():
+    mon = MongoClient("mongodb://127.0.0.1:27017")
+    collection = mon.logs.nginx
+    All = collection.count_documents({})
+    print("{} logs".format(All))
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, count))
+    status = collection.count_documents({"method": "GET", "path": "/status"})
+    print("{} status check".format(status))
+    if __name__ == "main":
+        count_methods()
