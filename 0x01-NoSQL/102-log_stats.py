d5b16bf236cd7bfa91c60acd9897339fb14ff6f6
e <e@g.com>
2024-03-26 09:02:58 -0700

    done

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 
 
 def methods_count():
-    """count methodb52d1642a4cb27be219c"""
+    """count method"""
     mon = MongoClient("mongodb://127.0.0.1:27017")
     collection = mon.logs.nginx
     All = collection.count_documents({})
@@ -16,7 +16,15 @@ def methods_count():
         print("\tmethod {}: {}".format(method, count))
     status = collection.count_documents({"method": "GET", "path": "/status"})
     print("{} status check".format(status))
-
-
+    ips = collection.aggregate(
+        [
+            {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+            {"$sort": {"count": -1}},
+            {"$limit": 10},
+        ]
+    )
+    print("IPs:")
+    for ip in ips:
+        print('\t{}: {}'.format(ip.get('_id'), ip.get("count")))
 if __name__ == "__main__":
     methods_count()
