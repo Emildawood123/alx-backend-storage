d40f4d08987cce99a391777fde9bb59fd7a73a6b
e <e@g.com>
2024-03-26 05:41:16 -0700

    done

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -2,4 +2,4 @@
 """find with ele in list"""
 def schools_by_topic(mongo_collection, topic):
     """find with ele in list"""
-    return mongo_collection.find({"topics.topic": "Python"})
+    return mongo_collection.find({"topics": topic})
