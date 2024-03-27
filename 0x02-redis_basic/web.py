70bc482b9c8ccb26f64d7cb387ae1d14a095b61a
Emildawood123 <edawood997@gmail.com>
2024-03-27 08:30:52 -0400

    add comment below fun

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -10,13 +10,14 @@ def helper_method(fn: Callable) -> Callable:
     """helper Method"""
     @wraps(fn)
     def wrapper(url: str) -> str:
+        """wrapper"""
         r = redis.Redis()
         r.incr(f"count:{url}")
         cached_page = r.get(f"{url}")
         if cached_page:
             return cached_page.decode("utf-8")
         response = fn(url)
-        r.set(f"{url}", response, 10)
+        r.set(f"{url}", response, ex=10)
         return response
 
     return wrapper
