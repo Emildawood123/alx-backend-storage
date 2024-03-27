eb63cfef4e63380cdf7c6c70ca13c9179a0a1cbd
Emildawood123 <edawood997@gmail.com>
2024-03-27 07:28:24 -0400

    without qoutes

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -11,10 +11,18 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    @count_calls
-    @call_history
     def store(self, data: str | bytes | int | float) -> str:
         """store method"""
-        keygen: str = uuid.uuid4()
-        self._redis.set(key, data)
+        keygen: str = str(uuid.uuid4())
+        self._redis.set(keygen, data)
         return keygen
+
+if __name__ == "__main__":
+    cache = Cache()
+
+    data = b"hello"
+    key = cache.store(data)
+    print(key)
+
+    local_redis = redis.Redis()
+    print(local_redis.get(key))
