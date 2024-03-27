973123268fbd61c3b4a5efaf3f5c5b7800f21a8b
Emildawood123 <edawood997@gmail.com>
2024-03-27 08:26:33 -0400

    change name to key

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""Advanced task"""
+import requests
+import redis
+from functools import wraps
+from typing import Callable
+
+
+def helper_method(fn: Callable) -> Callable:
+    """helper Method"""
+    @wraps(fn)
+    def wrapper(url: str) -> str:
+        r = redis.Redis()
+        r.incr(f"count:{url}")
+        cached_page = r.get(f"{url}")
+        if cached_page:
+            return cached_page.decode("utf-8")
+        response = fn(url)
+        r.set(f"{url}", response, 10)
+        return response
+
+    return wrapper
+
+
+@helper_method
+def get_page(url: str) -> str:
+    """get page method"""
+    response = requests.get(url)
