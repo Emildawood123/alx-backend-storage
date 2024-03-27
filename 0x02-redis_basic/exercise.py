9b5fe949d22b28145e501879345795be0245f25f
Emildawood123 <edawood997@gmail.com>
2024-03-27 07:41:57 -0400

    without qoutes

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -16,3 +16,19 @@ class Cache:
         keygen: str = str(uuid.uuid4())
         self._redis.set(keygen, data)
         return keygen
+
+    def get(self, key: str, fn: callable | None = None):
+        """get if we have fn"""
+        value = self._redis.get(key)
+        if fn is None:
+            return value
+        else:
+            return fn(value)
+
+    def get_str(self, key) -> str:
+        """get_str method"""
+        return self._redis.get(key, lambda x: x.decode('UTF-8'))
+
+    def get_int(self, key):
+        """get_int method"""
+        return self._redis.get(key, int)
